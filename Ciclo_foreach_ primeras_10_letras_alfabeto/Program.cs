e102deee3f0ecd75f7e4f934175b8916cb23e21e
Umblum12 <68798247+Umblum12@users.noreply.github.com>
2022-03-25 03:19:24 -0600

    Agregar archivos de proyecto.

---
+++ Ciclo_foreach_ primeras_10_letras_alfabeto/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+List<string> abecedario = new List<string>();
+abecedario.Add("A");
+abecedario.Add("B");
+abecedario.Add("C");
+abecedario.Add("D");
+abecedario.Add("E");
+abecedario.Add("F");
+abecedario.Add("G");
+abecedario.Add("H");
+abecedario.Add("I");
+abecedario.Add("J");
+foreach (string abecedarios in abecedario)
+{
+    Console.Write(abecedarios);
+}
+Console.ReadKey(true);
